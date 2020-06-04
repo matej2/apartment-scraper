5c0477abbd505c4ea78f22bf04d7c6f337812492
Matej2 <11059438+matej2@users.noreply.github.com>
2020-06-04 22:02:04 +0200

    Apartment model

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Apartment(models.Model):
+    url = models.CharField()
+    title = models.CharField()
+    rent = models.IntegerField()
+    contact = models.CharField()
+
+    def __str__(self):
+        return self.title + ": " + self.contact
