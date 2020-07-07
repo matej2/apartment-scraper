4a311cfaf3cef1e767b13d2af28255aeecb345aa
matej2 <11059438+matej2@users.noreply.github.com>
2020-07-07 21:18:56 +0200

    Installation fix

--- apartment_scraper/settings.py
+++ apartment_scraper/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scraper.apps.ScraperConfig',
-    'rest_framework.authtoken'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
