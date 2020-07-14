93e2f6170fd4be892c1dd0c60e988f4f68d8b76d
matej2 <11059438+matej2@users.noreply.github.com>
2020-07-14 21:28:07 +0200

    Google utilities

---
+++ scraper/GoogleUtilities.py
@@ -0,0 +1,25 @@
+def get_creds(SCOPES):
+    creds = None
+
+    if apartment == None:
+        return HttpResponse('Nothing to do', status=200)
+
+    # The file token.pickle is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
