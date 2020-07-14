330dbdc8dae7249ea62e87d61c05f3ed837634aa
matej2 <11059438+matej2@users.noreply.github.com>
2020-07-14 22:12:48 +0200

    Logging update

--- scraper/sceduler.py
+++ scraper/sceduler.py
@@ -23,11 +23,7 @@ def notify(str):
 
 
 def my_except_hook(exctype, value, traceback):
-    if exctype == KeyboardInterrupt:
-        print
-        "Handler code goes here"
-    else:
-        sys.__excepthook__(exctype, value, traceback)
+    notify(f'{exctype}: {value} | {traceback}')
 
 sys.excepthook = my_except_hook
 
