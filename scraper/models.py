980cbf1f174c747a8022cd7df57d2cba05a3350f
matej2 <11059438+matej2@users.noreply.github.com>
2020-07-16 15:01:59 +0200

    Revert "class meta"
    This reverts commit c7a55553

--- scraper/models.py
+++ scraper/models.py
@@ -15,9 +15,6 @@ class Apartment(models.Model):
     def __str__(self):
         return self.url
 
-    class Meta:
-        app_label = 'apt'
-
 class Listing(models.Model):
     url = models.CharField(max_length=500)
     limit = models.IntegerField(null=True, default=0)
@@ -26,6 +23,3 @@ class Listing(models.Model):
     title_selector = models.CharField(max_length=255, default='')
     rent_selector = models.CharField(max_length=255, default='')
     contact_selector = models.CharField(max_length=255, default='')
-
-    class Meta:
-        app_label = 'list'
