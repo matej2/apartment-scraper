c8d8846b10c089d3959d1faeb544063eec44f020
matej2 <11059438+matej2@users.noreply.github.com>
2020-09-01 16:02:23 +0200

    Django setup

--- apartment_scraper/scheduler.py
+++ apartment_scraper/scheduler.py
@@ -1,6 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+import os
+import django
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apartment_scraper.settings")
+django.setup()
 from scraper.common import main
 
 if __name__ == '__main__':
