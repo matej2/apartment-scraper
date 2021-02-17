0a942e95e17e4c23481c3d53d99ece7f83c58514
matej2 <11059438+matej2@users.noreply.github.com>
2021-02-17 16:04:56 +0100

    Service account credentials

--- scraper/GoogleUtilities.py
+++ scraper/GoogleUtilities.py
@@ -1,41 +1,19 @@
 import base64
-import json
 import os
-import pickle
+
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 from apartment_scraper.settings import BASE_DIR
 
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-
 
 def get_creds(SCOPES):
-    creds = None
     cred_dir = os.path.join(BASE_DIR, os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-    pickle_dir = os.path.join(BASE_DIR, 'token.pickle')
-
-    # The file token.pickle is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(pickle_dir):
-        with open(pickle_dir, 'rb') as token:
-            creds = pickle.load(token)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            config = json.loads(os.environ['GOOGLE_CREDENTIALS'])
-            flow = InstalledAppFlow.from_client_config(config, SCOPES)
-            host = os.environ['REDIRECT_URL'] if os.environ['REDIRECT_URL'] is not None else 'localhost'
-            #flow.redirect_uri = os.environ['REDIRECT_URL']
-            creds = flow.run_console()
-        # Save the credentials for the next run
-        with open(pickle_dir, 'wb') as token:
-            pickle.dump(creds, token)
-    return creds
+    credentials = service_account.Credentials.from_service_account_file(
+        cred_dir,
+        scopes=SCOPES
+    )
+    return credentials
 
 def add_contact(apartment):
     creds = get_creds(['https://www.googleapis.com/auth/contacts'])
